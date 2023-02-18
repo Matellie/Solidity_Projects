d2777fd53595e81008dd495f13c85b42bc406de9
Elie <62940355+Matellie@users.noreply.github.com>
2023-02-18 19:48:56 +0100

    Useless stacking contract

---
+++ Useless_Stacking.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
+
+import 'USLS.sol';
+
+contract Useless_Stacking {
+
+    KSIL public USLS;
+
+    mapping(address => uint) public balances;
+    uint16 public nbStackers;
+
+    mapping(address => uint) public timestamps;
+
+    constructor() {
+        USLS = new KSIL();
+        nbStackers = 0;
+    }
+
+    function stake() external payable {
+        require(msg.value > 0, "Invalid staking amount !");
+
+        if(balances[msg.sender] == 0) {
+            ++nbStackers;
+            balances[msg.sender] += msg.value;
+            timestamps[msg.sender] = block.timestamp;
+        } else {
+            claimRewards();
+            balances[msg.sender] += msg.value;
+        }
+    }
+
+    function unStake(uint _nbETH) external {
+        require(0 < _nbETH && _nbETH <= balances[msg.sender], "Invalid amount to un-stake !");
+
+        (bool sent,) = msg.sender.call{value: _nbETH}("");
+        require(sent, "An error has occured with the withdrawal !");
+
+        claimRewards();
+
+        balances[msg.sender] -= _nbETH;
+
+        if(balances[msg.sender] == 0) {
+            --nbStackers;
+            timestamps[msg.sender] = 0;
+        }
+    }
+
+    function claimRewards() public {
+        require(timestamps[msg.sender] > 0, "You do not have any rewards to claim !");
+
+        uint reward = (block.timestamp - timestamps[msg.sender]) * balances[msg.sender];
+
+        USLS.mint(msg.sender, reward);
+        timestamps[msg.sender] = block.timestamp;
+    }
+
+}
